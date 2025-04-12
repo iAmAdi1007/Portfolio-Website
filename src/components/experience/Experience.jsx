d5ec984dc6b469225db980b9e1843de0c00abe11
Aditya Pratap <rupalisingh@Adityas-MacBook-Air.local>
2025-04-13 01:27:09 +0530

    Added animation for header & refactor

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,6 +3,75 @@ import './experience.css';
 import { BsFillPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
+  const SKILLS_ARRAY = [
+    {
+      skill: 'React JS',
+      proficiency: 'Experienced'
+    },
+    {
+      skill: 'Next JS',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Javascript',
+      proficiency: 'Experienced'
+    },
+    {
+      skill: 'Typescript',
+      proficiency: 'Experienced'
+    },
+    {
+      skill: 'Redux Toolkit',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'React / Tanstack Query',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Tailwind CSS',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'HTML5',
+      proficiency: 'Experienced'
+    }
+  ]
+
+  const OTHER_SKILLS_ARRAY = [
+    {
+      skill: 'Data Structures',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Algorithms',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Jest',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'React Testing Library',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Webpack',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Cypress JS',
+      proficiency: 'Beginner'
+    },
+    {
+      skill: 'Node JS',
+      proficiency: 'Intermediate'
+    },
+    {
+      skill: 'Git',
+      proficiency: 'Experienced'
+    } 
+  ]
   return (
     <section id="experience">
       <h5>What skills I have</h5>
@@ -11,82 +80,28 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Javascript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>React JS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Tailwind</h4>
-                <small className='text-light'>Beginner</small>
-              </div>
-            </article>
+            {SKILLS_ARRAY.map((skill, idx) =>
+              <article className='experience__details' key={idx}>
+                <BsFillPatchCheckFill className='experience__details-icon' />
+                <div>
+                  <h4>{skill.skill}</h4>
+                  <small className='text-light'>{skill.proficiency}</small>
+                </div>
+              </article>)}
           </div>
         </div>
         <div className="experience__backend">
           <h3>Some Other Skills and Technologies</h3>
           <div className="experience__content">
-            <article className='experience__details'>
+            {OTHER_SKILLS_ARRAY.map((skill, index) => (
+              <article className='experience__details'>
               <BsFillPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Data Structures</h4>
-                <small className='text-light'>Intermediate</small>
+                <h4>{skill.skill}</h4>
+                <small className='text-light'>{skill.proficiency}</small>
               </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Algorithms</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Node JS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Express JS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Mongo DB</h4>
-                <small className='text-light'>Beginner</small>
-              </div>
-            </article>
-
+            </article>)
+          )}
           </div>
         </div>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CTA from './CTA';
 import './header.css';
 import ME from '../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
+  const USER_NAME = "Aditya Pratap";
+  const PROFESSION_ARRAY = [
+    'Front-End Developer',
+    'Web Developer',
+    'UI Developer'
+  ]
+
+  const [index, setIndex] = useState(0);
+  const [name, setName] = useState('');
+  const [professionValue, setProfessionValue] = useState('');
+  const [isTitleRendered, setIsTitleRendered] = useState(false);
+  const [professionIndex, setProfessionIndex] = useState(0);
+  const [professionLetterIndex, setProfessionLetterIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < USER_NAME.length) {
+      const intervalId = setInterval(() => {
+        setName(prev => prev + USER_NAME[index]);
+        setIndex(index + 1);
+      }, 100)
+
+      return () => clearInterval(intervalId);
+    }else{
+      const delayId = setTimeout(() => {
+        setIsTitleRendered(true);
+      }, 400)
+
+      return () => clearTimeout(delayId);
+    }
+  },[index, USER_NAME, PROFESSION_ARRAY]);
+
+  useEffect(() => {
+    if(!isTitleRendered || professionIndex >= PROFESSION_ARRAY.length) return;
+
+    const currentWord = PROFESSION_ARRAY[professionIndex];
+    if(professionLetterIndex < currentWord.length){
+      const id = setInterval(() => {
+        setProfessionValue(prev => prev + currentWord[professionLetterIndex]);
+        setProfessionLetterIndex(professionLetterIndex + 1);
+      }, 100)
+
+      return () => clearInterval(id);
+    }else{
+      const timeout = setTimeout(() => {
+        setProfessionValue('');
+        setProfessionLetterIndex(0);
+        setProfessionIndex(prev => {
+          if(prev === PROFESSION_ARRAY.length - 1){
+            return 0
+          }else{
+            return prev + 1;
+          }
+        })
+      }, 400)
+
+      return () => clearTimeout(timeout);
+    }
+
+  },[isTitleRendered, professionLetterIndex, professionIndex, PROFESSION_ARRAY])
+
+
+
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Aditya Pratap</h1>
-        <h5 className='text-light'>Front-End Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
+        <h4>Hello I'm</h4>
+        <h1 className='username'>{name} <span className='cursor__animation' /></h1>
+        <h3 className='text-light' style={{height: '2rem'}}>{professionValue}</h3>
+        <CTA />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
